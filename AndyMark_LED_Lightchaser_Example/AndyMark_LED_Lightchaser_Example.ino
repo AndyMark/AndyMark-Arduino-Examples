3272cc2d8aa877f925e04ab4dd396bb85c3de50d
craig-am <craig@andymark.com>
2013-12-04 18:07:13 -0500

    First draft of setup document

--- AndyMark_LED_Lightchaser_Example/AndyMark_LED_Lightchaser_Example.ino
+++ AndyMark_LED_Lightchaser_Example/AndyMark_LED_Lightchaser_Example.ino
@@ -1,7 +1,7 @@
 //This is a mashup of FastSPI_LED2 firstlight example and some other light chase hacks for the AM-2640 5V, Addressable LED strips http://www.andymark.com/product-p/am-2640.htm
 //We ran this demo off of our AM-2287 ethernet arduino's http://www.andymark.com/product-p/am-2287.htm
 //DO NOT try to power the whole strip off the arduino 5v regulator.  
-//At full bright white, the strip can draw 1.5Amps or so. This will overheat the regulator.
+//At full bright white, the strip can draw 1.5Amps or so. This will overheat or burnout the regulator.
 //We recommend running these led strips off of the AM-0899 10-30Vin to 5V 5A out stepdown converter http://www.andymark.com/product-p/am-0899.htm
 //AndyMark, Inc.
 //Craig Kessler 12/3/2013
@@ -28,90 +28,90 @@
 CRGB leds[NUM_LEDS];
 
 // This function sets up the leds and tells the controller about them
-void setup() {
-	// sanity check delay - allows reprogramming if accidentally blowing power w/leds
-	delay(2000);
+void setup() 
+{
+  // Uncomment one of the following lines for your leds arrangement.
+  // FastLED.addLeds<TM1803, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<TM1804, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<TM1809, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<UCS1903, DATA_PIN, RGB>(leds, NUM_LEDS);
 
-	// Uncomment one of the following lines for your leds arrangement.
-	// FastLED.addLeds<TM1803, DATA_PIN, RGB>(leds, NUM_LEDS);
-	// FastLED.addLeds<TM1804, DATA_PIN, RGB>(leds, NUM_LEDS);
-	// FastLED.addLeds<TM1809, DATA_PIN, RGB>(leds, NUM_LEDS);
-	//FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
-	// FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
-	// FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
-	// FastLED.addLeds<UCS1903, DATA_PIN, RGB>(leds, NUM_LEDS);
-
+  //This is the chipset in the AM-2640 LED strip
   FastLED.addLeds<WS2801, RGB>(leds, NUM_LEDS);
-  
-	// FastLED.addLeds<SM16716, RGB>(leds, NUM_LEDS);
-	// FastLED.addLeds<LPD8806, RGB>(leds, NUM_LEDS);
 
-	//FastLED.addLeds<WS2801, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-	// FastLED.addLeds<SM16716, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-	// FastLED.addLeds<LPD8806, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<SM16716, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<LPD8806, RGB>(leds, NUM_LEDS);
+
+  // FastLED.addLeds<WS2801, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<SM16716, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<LPD8806, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
 }
 
 // This function runs over and over, and is where you do the magic to light
 // your leds.
 void loop()
 {
-//https://code.google.com/p/fastspi/wiki/CRGBreference	
-	color_chase(CRGB::Green, 10);
-	color_chase(CRGB::BlueViolet, 10);
-	CRGB temp_rgb;
-	CHSV hsv(215,255,255);
-	hsv2rgb_rainbow(hsv, temp_rgb);
-	missing_dot_chase(CRGB::White, 25);
-	missing_dot_chase(CRGB::Red, 25);
-	missing_dot_chase(CRGB::Yellow, 25);
-	missing_dot_chase(CRGB::Green, 25);
-	missing_dot_chase(CRGB::Cyan, 25);
-	missing_dot_chase(CRGB::Blue, 25);
-	missing_dot_chase(0x3000cc, 25) ;
+  //https://code.google.com/p/fastspi/wiki/CRGBreference	
+  color_chase(CRGB::Green, 10);
+  color_chase(CRGB::BlueViolet, 10);
+  CRGB temp_rgb;
+  CHSV hsv(215,255,255);
+  hsv2rgb_rainbow(hsv, temp_rgb);
+  missing_dot_chase(CRGB::White, 25);
+  missing_dot_chase(CRGB::Red, 25);
+  missing_dot_chase(CRGB::Yellow, 25);
+  missing_dot_chase(CRGB::Green, 25);
+  missing_dot_chase(CRGB::Cyan, 25);
+  missing_dot_chase(CRGB::Blue, 25);
+  missing_dot_chase(0x3000cc, 25) ;
 }
 
 void color_chase(uint32_t color, uint8_t wait)
 {	
-	FastLED.clear();
-	FastLED.setBrightness(100);
-	// Move a single led
-	for(int led_number = 0; led_number < NUM_LEDS; led_number++)
-	{
-		// Turn our current led ON, then show the leds
-		leds[led_number] = color;
+  FastLED.clear();
+  FastLED.setBrightness(100);
+  // Move a single led
+  for(int led_number = 0; led_number < NUM_LEDS; led_number++)
+  {
+    // Turn our current led ON, then show the leds
+    leds[led_number] = color;
 
-		// Show the leds (only one of which is set to white, from above)
-		FastLED.show();
+    // Show the leds (only one of which is set to white, from above)
+    FastLED.show();
 
-		// Wait a little bit
-		delay(50);
+    // Wait a little bit
+    delay(50);
 
-		// Turn our current led back to black for the next loop around
-		leds[led_number] = CRGB::Black;
-	}
+    // Turn our current led back to black for the next loop around
+    leds[led_number] = CRGB::Black;
+  }
 }
 
 //Move an "empty" dot down the strip
 void missing_dot_chase(uint32_t color, uint8_t wait)
 {
-	int led_number;
+  int led_number;
 
-	for (int led_brightness = 100; led_brightness > 10; led_brightness/=2)
-	{
-		FastLED.setBrightness(led_brightness);
-		// Start by turning all pixels on:
-		for(led_number = 0; led_number < NUM_LEDS; led_number++) leds[led_number] = color;
+  for (int led_brightness = 100; led_brightness > 10; led_brightness/=2)
+  {
+    FastLED.setBrightness(led_brightness);
+    // Start by turning all pixels on:
+    for(led_number = 0; led_number < NUM_LEDS; led_number++) leds[led_number] = color;
 
-		// Then display one pixel at a time:
-		for(led_number = 0; led_number < NUM_LEDS; led_number++)
-		{
-			leds[led_number] = CRGB::Black; // Set new pixel 'off'
-			if( led_number > 0 && led_number < NUM_LEDS)
-			{
-				leds[led_number-1] = color; // Set previous pixel 'on'
-			}
-			FastLED.show();
-			delay(wait);
-		}
-	}
+    // Then display one pixel at a time:
+    for(led_number = 0; led_number < NUM_LEDS; led_number++)
+    {
+      leds[led_number] = CRGB::Black; // Set new pixel 'off'
+      if( led_number > 0 && led_number < NUM_LEDS)
+      {
+        leds[led_number-1] = color; // Set previous pixel 'on'
+      }
+      FastLED.show();
+      delay(wait);
+    }
+  }
 }
+
--- AndyMark_Simple_Lightsaber_Example/AndyMark_Simple_Lightsaber_Example.ino
+++ AndyMark_Simple_Lightsaber_Example/AndyMark_Simple_Lightsaber_Example.ino
@@ -31,31 +31,28 @@ CRGB leds[NUM_LEDS];
 // This function sets up the leds and tells the controller about them
 void setup()
 {
-	// Uncomment one of the following lines for your leds arrangement.
-	// FastLED.addLeds<TM1803, DATA_PIN, RGB>(leds, NUM_LEDS);
-	// FastLED.addLeds<TM1804, DATA_PIN, RGB>(leds, NUM_LEDS);
-	// FastLED.addLeds<TM1809, DATA_PIN, RGB>(leds, NUM_LEDS);
-	//FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
-	// FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
-	// FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
-	// FastLED.addLeds<UCS1903, DATA_PIN, RGB>(leds, NUM_LEDS);
-        
-        //This is the chipset in the AM-2640 LED strip
-	FastLED.addLeds<WS2801, RGB>(leds, NUM_LEDS);
+  // Uncomment one of the following lines for your leds arrangement.
+  // FastLED.addLeds<TM1803, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<TM1804, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<TM1809, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<UCS1903, DATA_PIN, RGB>(leds, NUM_LEDS);
 
-	// FastLED.addLeds<SM16716, RGB>(leds, NUM_LEDS);
-	// FastLED.addLeds<LPD8806, RGB>(leds, NUM_LEDS);
+  //This is the chipset in the AM-2640 LED strip
+  FastLED.addLeds<WS2801, RGB>(leds, NUM_LEDS);
 
-	//FastLED.addLeds<WS2801, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-	// FastLED.addLeds<SM16716, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-	// FastLED.addLeds<LPD8806, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-	////SdPlay.setSDCSPin(10); // Enable if your SD card CS-Pin is not at Pin 4...
-	//SdPlay.init(SSDA_MODE_FULLRATE | SSDA_MODE_MONO | SSDA_MODE_AUTOWORKER);
-	//SdPlay.setFile("lightsab.wav");
-	
-        //Change the color value for the lightsaber color of your choice.  
-        //The last number controls how long the saber takes to complete the lighting sequence.
-	light_saber_on(CRGB::Green, 35);
+  // FastLED.addLeds<SM16716, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<LPD8806, RGB>(leds, NUM_LEDS);
+
+  // FastLED.addLeds<WS2801, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<SM16716, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<LPD8806, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+
+  //Change the color value for the lightsaber color of your choice.  
+  //The last number controls how long the saber takes to complete the lighting sequence.
+  light_saber_on(CRGB::Green, 35);
 }
 
 // This function runs over and over, and is where you do the magic to light
@@ -67,36 +64,37 @@ void loop()
 
 void light_saber_on(uint32_t c, uint8_t wait)
 {
-	int led_number = 0;
-	FastLED.clear();
-	FastLED.setBrightness(5);
-		
-	for(led_number = 0; led_number < NUM_LEDS; led_number++)
-	{
-		delay(wait);
-		leds[led_number] = c;
-		FastLED.show();
-		//FastLED.show_one(led_number, led_brightness);
-	}
-	//Increase "beam" intensity with "power flicker"
-	for (int led_brightness = 5; led_brightness <= 100; led_brightness+=5  )
-	{
-		FastLED.setBrightness(led_brightness);
-		FastLED.show();
-		delay(40);
-	}
-	for (int led_brightness = 100; led_brightness >= 50; led_brightness-=5  )
-	{
-		FastLED.setBrightness(led_brightness);
-		FastLED.show();
-		delay(30);
-	}	
-	for (int led_brightness = 50; led_brightness <= 100; led_brightness+=5  )
-	{
-		FastLED.setBrightness(led_brightness);
-		FastLED.show();
-		delay(25);
-	}
-	return;
+  int led_number = 0;
+  FastLED.clear();
+  FastLED.setBrightness(5);
+
+  for(led_number = 0; led_number < NUM_LEDS; led_number++)
+  {
+    delay(wait);
+    leds[led_number] = c;
+    FastLED.show();
+    //FastLED.show_one(led_number, led_brightness);
+  }
+  //Increase "beam" intensity with "power flicker"
+  for (int led_brightness = 5; led_brightness <= 100; led_brightness+=5  )
+  {
+    FastLED.setBrightness(led_brightness);
+    FastLED.show();
+    delay(40);
+  }
+  for (int led_brightness = 100; led_brightness >= 50; led_brightness-=5  )
+  {
+    FastLED.setBrightness(led_brightness);
+    FastLED.show();
+    delay(30);
+  }	
+  for (int led_brightness = 50; led_brightness <= 100; led_brightness+=5  )
+  {
+    FastLED.setBrightness(led_brightness);
+    FastLED.show();
+    delay(25);
+  }
+  return;
 }
+
 
